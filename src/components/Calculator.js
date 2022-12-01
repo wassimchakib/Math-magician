0693c374b39430e52bc3fd2bde3f1b827dd47ffc
Wassim <wassimchakib97@gmail.com>
2022-12-01 13:00:14 +0100

    Refactor Calculator.js to use Hooks instead of Class component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,52 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 import Btn from './Btn';
 import Display from './Display';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const Calculator = () => {
+  const [item, setItem] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  handleClick(e) {
-    const newObj = calculate(this.state, e.target.textContent);
-    this.setState(newObj);
-  }
+  const handleClick = (e) => {
+    setItem(calculate(item, e.target.textContent));
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="calculator">
-        <Display className="result" result={next || operation || total || '0'} />
-        <Btn value="AC" className="left-btns" onClick={this.handleClick} />
-        <Btn value="+/-" className="left-btns" onClick={this.handleClick} />
-        <Btn value="%" className="left-btns" onClick={this.handleClick} />
-        <Btn value="÷" className="right-btns" onClick={this.handleClick} />
-        <Btn value="7" className="left-btns" onClick={this.handleClick} />
-        <Btn value="8" className="left-btns" onClick={this.handleClick} />
-        <Btn value="9" className="left-btns" onClick={this.handleClick} />
-        <Btn value="x" className="right-btns" onClick={this.handleClick} />
-        <Btn value="4" className="left-btns" onClick={this.handleClick} />
-        <Btn value="5" className="left-btns" onClick={this.handleClick} />
-        <Btn value="6" className="left-btns" onClick={this.handleClick} />
-        <Btn value="-" className="right-btns" onClick={this.handleClick} />
-        <Btn value="1" className="left-btns" onClick={this.handleClick} />
-        <Btn value="2" className="left-btns" onClick={this.handleClick} />
-        <Btn value="3" className="left-btns" onClick={this.handleClick} />
-        <Btn value="+" className="right-btns" onClick={this.handleClick} />
-        <Btn value="0" className="left-btns zero-btn" onClick={this.handleClick} />
-        <Btn value="." className="left-btns" onClick={this.handleClick} />
-        <Btn value="=" className="right-btns" onClick={this.handleClick} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="calculator">
+      <Display className="result" result={item.next || item.operation || item.total || '0'} />
+      <Btn value="AC" className="left-btns" onClick={handleClick} />
+      <Btn value="+/-" className="left-btns" onClick={handleClick} />
+      <Btn value="%" className="left-btns" onClick={handleClick} />
+      <Btn value="÷" className="right-btns" onClick={handleClick} />
+      <Btn value="7" className="left-btns" onClick={handleClick} />
+      <Btn value="8" className="left-btns" onClick={handleClick} />
+      <Btn value="9" className="left-btns" onClick={handleClick} />
+      <Btn value="x" className="right-btns" onClick={handleClick} />
+      <Btn value="4" className="left-btns" onClick={handleClick} />
+      <Btn value="5" className="left-btns" onClick={handleClick} />
+      <Btn value="6" className="left-btns" onClick={handleClick} />
+      <Btn value="-" className="right-btns" onClick={handleClick} />
+      <Btn value="1" className="left-btns" onClick={handleClick} />
+      <Btn value="2" className="left-btns" onClick={handleClick} />
+      <Btn value="3" className="left-btns" onClick={handleClick} />
+      <Btn value="+" className="right-btns" onClick={handleClick} />
+      <Btn value="0" className="left-btns zero-btn" onClick={handleClick} />
+      <Btn value="." className="left-btns" onClick={handleClick} />
+      <Btn value="=" className="right-btns" onClick={handleClick} />
+    </div>
+  );
+};
 
 export default Calculator;

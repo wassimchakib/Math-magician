8a9746dcadb9e7bea6d2d4f6d5805f9a0ac9a720
Wassim <wassimchakib97@gmail.com>
2022-12-07 14:02:35 +0100

    Add quote page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
-import Calculator from './components/calculator/Calculator';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Home from './components/Home';
+import Calculator from './components/calculator/Calculator';
+import Quote from './components/Quote';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Router>
+      <Routes>
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  </div>
+);
 
 export default App;
